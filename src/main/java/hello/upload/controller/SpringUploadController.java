eb1d07ad57f6f69b91da880024b637625812adb8
esy <yesman5307@gmail.com>
2022-11-30 07:39:44 +0900

    commit

---
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -0,0 +1,14 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/spring")
+public class SpringUploadController {
+
+    @Value()
+}
